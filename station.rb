b91e805b36b762786a28522a8438249a4b28180a
katelarionova2058 <92648690+katelarionova2058@users.noreply.github.com>
2021-12-03 21:07:07 +0300

    Update station.rb

--- station.rb
+++ station.rb
@@ -1,1 +1,40 @@
+require_relative 'instance_counter.rb'
 
+class Station
+
+  include InstanceCounter
+
+  @@all = []
+
+  attr_reader :name, 
+              :trains_on_station
+
+  def initialize(name)
+    @@all.push(self)
+    @name = name  
+    @trains_on_station = []
+    register_instance
+  end
+
+  def add_trains (train)
+    @trains_on_station << train
+  end
+
+  def del_trains (train)
+    trains_on_station.delete(train)
+  end
+
+  def show_trains
+    @trains_on_station.each { |train| puts train.name }
+  end
+
+  def return_type(type)
+    count_of_type = @trains_on_station.select { |train| train.type == type }
+    count_of_type.count
+  end
+
+  def self.all
+    @@all
+  end
+
+end
